cb8adec3cb2940da6e35d539ce71878f1df03a40
Oleg Atrohov <oleg/atrokhau@gmail.com>
2023-11-01 19:03:12 +0300

    Testing footer functionality

--- elements/footer_element.py
+++ elements/footer_element.py
@@ -1,5 +1,7 @@
 import allure
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from helpers.base import BasePage
 
 
@@ -22,11 +24,43 @@ class FooterElement(BasePage):
     ELEMENT_IN_BLOG_PAGE = '//div[h1[text()="Оллспортс Статьи"]]/article'
     ACT_RESULT = '//div/div/main/section[1]/p[7]/a'
     COMPANIES_PAGE = '//h1[@class="top_title"]'
-    DOCUMENTS_FOR_LEGAL_ENTITIES = "//h2[text()='Документы для юридических лиц']"
+    DOCUMENTS_FOR_LEGAL_ENTITIES = "//a[contains(text(), 'Документы для юр.лиц')]"
     EXPECTED_RESULT_TEXT = '+375 (44) 502-36-13'
     FILE_NAME = "Индивидуальные лицензии"
     PHONE_NUMBER = '+375 (44) 525-38-92'
     EXPECTED_ALERT_TEXT = "Открыть приложение 'Выбор приложения'"
+
+    READ_FILE_1 = "//div[@class='license'][1]//a[text()='Читать документ']"
+    READ_FILE_2 = "//div[@class='license'][2]//a[text()='Читать документ']"
+    READ_FILE_3 = "//div[@class='license'][3]//a[text()='Читать документ']"
+    ELEMENT_IN_LICENSES_DOC = "//h1[text()='ПОЛЬЗОВАТЕЛЬСКОЕ СОГЛАШЕНИЕ держателей Карт Allsports']"
+    ELEMENT_IN_PERSONAL_INFORMATION = "//h2[text()='Политика в отношении обработки персональных данных']"
+
+    ELEMENT_IN_PERSONAL_INFORMATION_DOC_FOR_TEXT = "//h2[text()='Политика в отношении обработки персональных данных']"
+    TEXT = "Политика в отношении обработки персональных данных"
+
+    CHECK_DATE = "//small[text()='18.02.2022']"
+    TEXT_DATE = "18.02.2022"
+
+    DATE_2 = "//a[contains(@href, '220127_processing_personal_data')]"
+    CHECK_DATE_2 = "//small[text()='27.01.2022']"
+    TEXT_DATE_2 = "27.01.2022"
+
+    DATE_3 = "//a[contains(@href, '211115_policy')]"
+    CHECK_DATE_3 = "//small[text()='15.11.2021']"
+    TEXT_DATE_3 = "15.11.2021"
+
+    ELEMENT_IN_ACCESS_RULES = '//h1[text()="Правила доступа в спортивные объекты с помощью Электронной платформы Allsports"]'
+
+    READ_FILE_ACCESS_RULES_1 = "//div[@class='license'][1]//a[text()='Читать документ']"
+    READ_FILE_ACCESS_RULES_2 = "//div[@class='license'][2]//a[text()='Читать документ']"
+
+    DOCUMENTS_FOR_LEGAL = "//a[contains(text(), 'Документы для юр.лиц')]"
+
+    DATE_ACCESS_2 = "//a[contains(@href, '220127_processing_personal_data')]"
+    DATE_ACCESS_3 = "//a[contains(@href, '211115_policy')]"
+
+    ELEMENT_IN_ACCESS_RULES_2 = "//h2[text()='Документы для юридических лиц']"
 
     def __init__(self, driver):
         self.driver = driver
@@ -82,25 +116,36 @@ class FooterElement(BasePage):
 
     @allure.step("Assert dok")
     def assert_dok(self, file):
-        if file in self.driver.page_source:
-            print(f"Текст '{file}' найден на странице.")
+        documents = {
+            "Индивидуальные лицензии": "/by/individual_license/220316_license/",
+            "Политика в отношении обработки персональных данных": "/by/policy/220218_processing_personal_data/",
+            "Правила доступа в спортивные объекты": "/by/rule/230801_rule/"
+        }
+
+        if file in documents:
+            link = documents[file]
+            try:
+                element_present = EC.presence_of_element_located((By.XPATH, f"//a[@href='{link}']"))
+                WebDriverWait(self.driver, 10).until(element_present)
+                print(f"Документ '{file}' найден на странице.")
+            except:
+                print(f"Ссылка на документ '{file}' не найдена на странице.")
+                assert False, f"Ссылка на документ '{file}' не найдена на странице."
         else:
-            print(f"Текст '{file}' не найден на странице.")
-        assert file in self.driver.page_source
+            print(f"Документ '{file}' не существует.")
+            assert False, f"Документ '{file}' не существует."
 
     @allure.step("Assert Instagram")
     def assert_instagram(self):
-        self.wait_for_visible(self.INSTAGRAM)
         current_url = self.get_current_url()
         print("Текущий URL (instagram):", current_url)
         assert self.get_current_url() == 'https://www.instagram.com/allsports.fit/'
 
     @allure.step("Assert LinkedIn")
-    def assert_linkedin(self):
-        self.wait_for_visible(self.LINKEDIN)
+    def assert_LinkedIn(self):
         current_url = self.get_current_url()
         print("Текущий URL (LinkedIn):", current_url)
-        assert current_url == 'https://www.linkedin.com/company/allsportsby'
+        assert self.get_current_url() == 'https://www.linkedin.com/company/allsportsby'
 
     @allure.step("Assert phone numbers and email")
     def assert_phone_numbers_email(self):
@@ -134,3 +179,144 @@ class FooterElement(BasePage):
     @allure.step("Check the 'Companies' page")
     def assert_companies_page(self):
         self.wait_for_element_is_displayed(self.COMPANIES_PAGE)
+
+    @allure.step("Switch to New Window instagram")
+    def switch_to_new_window_with_instagram(self):
+        self.switch_to_new_window()
+
+    @allure.step("Switch to New Window linkedin")
+    def switch_to_new_window_with_linkedin(self):
+        self.switch_to_new_window()
+
+    @allure.step("Click on read document first")
+    def click_on_docs(self):
+        self.hard_click(self.READ_FILE_1)
+
+    @allure.step("Click on read document second")
+    def click_on_docs(self):
+        self.hard_click(self.READ_FILE_2)
+
+    @allure.step("Click on read document three")
+    def click_on_docs(self):
+        self.hard_click(self.READ_FILE_3)
+
+    @allure.step("Click on read document first")
+    def click_on_read_buttom(self):
+        self.hard_click(self.READ_FILE_1)
+
+    @allure.step("Click on 'Read 2' button")
+    def click_on_read_2_buttom(self):
+        self.hard_click(self.READ_FILE_2)
+
+    @allure.step("Open 'Individual Licenses' document")
+    def open_doc_individual_licenses(self):
+        self.wait_for_element_is_displayed(self.ELEMENT_IN_LICENSES_DOC)
+
+    @allure.step("Assert 'Individual Licenses' document URL")
+    def assert_doc_individual_licenses_url(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (individual licenses):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/individual_license/220316_license/'
+
+    @allure.step("Open 'Personal Information' document")
+    def open_doc_personal_information(self):
+        self.wait_for_element_is_displayed(self.ELEMENT_IN_PERSONAL_INFORMATION)
+
+    @allure.step("Check text in 'Personal Information' document")
+    def text_checking(self):
+        self.assert_text_in_element(self.ELEMENT_IN_PERSONAL_INFORMATION_DOC_FOR_TEXT, self.TEXT)
+
+    @allure.step("Assert 'Personal Information' document URL")
+    def assert_doc_personal_information_url(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (personal information):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/policy/220218_processing_personal_data/'
+
+    @allure.step("Assert text for date checking")
+    def assert_text_checking_date(self):
+        self.assert_text_in_element(self.CHECK_DATE, self.TEXT_DATE)
+
+    @allure.step("Click on Date 2")
+    def click_on_date_2(self):
+        self.hard_click(self.DATE_2)
+
+    @allure.step("Assert text for date 2 checking")
+    def assert_text_checking_date_2(self):
+        self.assert_text_in_element(self.CHECK_DATE_2, self.TEXT_DATE_2)
+
+    @allure.step("Click on Date 3")
+    def click_on_date_3(self):
+        self.hard_click(self.DATE_3)
+
+    @allure.step("Assert text for Date 3 checking")
+    def assert_text_checking_date_3(self):
+        self.assert_text_in_element(self.CHECK_DATE_3, self.TEXT_DATE_3)
+
+    @allure.step("Click on 'Read' button 3")
+    def click_on_read_buttom_3(self):
+        self.hard_click(self.READ_FILE_3)
+
+    @allure.step("Open 'Access Rules' document")
+    def open_doc_access_rules(self):
+        self.wait_for_element_is_displayed(self.ELEMENT_IN_ACCESS_RULES)
+
+    @allure.step("Assert 'Access Rules' document URL")
+    def assert_doc_access_rules_url(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (access rules):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/rule/230801_rule/'
+
+    def click_on_legal_documents_personal_information(self):
+        self.hard_click(self.DATE_3)
+
+    @allure.step("Click on 'Read' button for Access Rules")
+    def click_on_read_buttom_access_rules(self):
+        self.hard_click(self.READ_FILE_ACCESS_RULES_1)
+
+    @allure.step("Click on 'Read' button for Access Rules 2")
+    def click_on_read_buttom_access_rules_2(self):
+        self.hard_click(self.READ_FILE_ACCESS_RULES_2)
+
+    @allure.step("Click on Date 2 for Access Rules")
+    def click_on_date_2_access_rules(self):
+        self.hard_click(self.READ_FILE_ACCESS_RULES_1)
+
+    @allure.step("Assert 'Access Rules' URL")
+    def assert_access_rules_url(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (22.02.18):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/policy/220218_processing_personal_data/'
+
+    @allure.step("Click on Date 2 for Access")
+    def click_on_date_2_access(self):
+        self.hard_click(self.DATE_ACCESS_2)
+
+    @allure.step("Assert 'Access Rules' URL 1")
+    def assert_access_rules_url_1(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (22.01.27):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/policy/220127_processing_personal_data/'
+
+    @allure.step("Click on Date 3 for Access")
+    def click_on_date_3_access(self):
+        self.hard_click(self.DATE_ACCESS_3)
+
+    @allure.step("Assert 'Access Rules' URL 2")
+    def assert_access_rules_url_2(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (21.11.15):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/policy/211115_policy/'
+
+    @allure.step("Click on 'Read' button 2 for Access Rules")
+    def click_on_read_buttom_2_access_rules(self):
+        self.hard_click(self.READ_FILE_ACCESS_RULES_2)
+
+    @allure.step("Assert 'Access Rules' and Element URL 2")
+    def assert_access_rules_and_alement_url_2(self):
+        current_url = self.get_current_url()
+        print("Текущий URL (license):", current_url)
+        assert self.get_current_url() == 'https://www.allsports.fit/by/license/230306_license/'
+
+    @allure.step("Open 'Access Rules' Page 2")
+    def open_doc_access_rules_page_2(self):
+        self.wait_for_element_is_displayed(self.ELEMENT_IN_ACCESS_RULES_2)
