c7162283a5b99c2310e3814e57f779e9a1540896
Oleg Atrohov <oleg/atrokhau@gmail.com>
2023-11-08 13:09:45 +0300

    add locators

---
+++ locators/__init__.py
@@ -0,0 +1,1 @@
+from locators.for_objects_page import ObjectsPageLocators
--- pages/objects_page.py
+++ pages/objects_page.py
@@ -5,32 +5,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 from helpers.base import BasePage
+from locators import ObjectsPageLocators
 
 
-class ObjectPage(BasePage):
-    BUTTON_OBJECT_TAB = '//ul/li/a[text()="Объекты"]'
-    EXPECTED_URL = 'https://www.allsports.fit/by/objects/'
-    TEXT_ON_OBJECT_PAGE = '//h3[text()="Ваши вопросы и ответы"]'
-    TEXT_ON_OBJECT_PAGE_2 = '//h3[text()="Подписка в приложении Allsports выгоднее многих абонементов"]'
-    TEXT_ON_OBJECT_PAGE_3 = '//h3[text()="Начните заниматься спортом со своими коллегами!"]'
-    TEXT_ON_OBJECT_PAGE_4 = '//button[text()="Получить предложение"]'
-    BUTTON_CALL_PUSH = '//a[@class="button blue" and text()="Позвоните нам"]'
-
-    NAME_COMPANY = '//input[@name="company"]'
-
-    NUMBER_EMPLOYE_LOCATOR = '//div[@class="select"]//button'
-    TEXT_NUMBER_EMPLOYE = '//button[text()="от 100 до 1000"]'
-
-    PHONE_NUMBER = '//input[@name="phone"]'
-    CITY = '//input[@name="city"]'
-    NAME_EMPLOY = '//section[2]/form/div[4]/input'
-    EMAIL = '//input[@name="email"]'
-
-    SUBSCRIPTION_LOCATOR = '//button[contains(text(), "Тип подписки")]'
-    GOLD_LEVEL = '//*[@id="gatsby-focus-wrapper"]/main/div/div/div/main/section[1]/div[2]/div[1]/div[1]'
-    INPUT_SEARCH = '//main/section[1]/form/div[2]/div[2]/input'
-    SUPPLIER_LOCATOR = '//button[@class="result" and text()="Бассейн Гимназии 14"]'
-    FOUND_ELEMENT = '//main/section[1]/section/section/h1'
+class ObjectPage(BasePage, ObjectsPageLocators):
 
     def __init__(self, driver):
         self.text_company_email = 'test@gmail.com'
